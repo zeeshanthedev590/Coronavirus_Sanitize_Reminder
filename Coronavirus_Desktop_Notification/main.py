e8aee5555a94c4c317a14c35278c51a47690d091
zeeshanthedev590 <62704414+zeeshanthedev590@users.noreply.github.com>
2020-08-21 20:58:55 +0500

    Add files via upload

---
+++ Coronavirus_Desktop_Notification/main.py
@@ -0,0 +1,19 @@
+from plyer import notification
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+def notifyme(title, message):
+    notification.notify(
+        title=title,
+        message=message,
+        app_icon='D:\My Projects\Python Projects\Coronavirus_Desktop_Notification\img.ico',
+        timeout=15
+
+    )
+
+
+if __name__ == "__main__":
+    notifyme("Lets Stop The Spread Of This Virus", "Sanitize Your Hands")
+    time.sleep(1800)
